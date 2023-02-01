d57b47bba16395ecb2cbb1655c248ba50ff29e4d
wasabi315 <wasabi.nvim.st@gmail.com>
2023-02-01 21:48:56 +0100

    Refactor prover

--- lib/prover.ml
+++ lib/prover.ml
@@ -8,113 +8,108 @@ let ( let* ) o f =
 
 and return x = Some x
 
-(*
-  pop_matched f ({p} ∪ P) = Some (x, P)  (if f p = Some x)
-  pop_matched f P = None                 (otherwise)
-*)
-let pop_matched (f : Prop.t -> 'a option) (props : Prop_set.t) =
-  let value_ref = ref None in
-  let g prop =
-    match !value_ref, f prop with
-    | Some _, _ | None, None -> Some prop
+(* pop_matched f ({p} ∪ P) = Some (x, P)  (if f p = Some x)
+   pop_matched f P = None                 (otherwise) *)
+let pop_matched matcher props =
+  let matched = ref None in
+  let choose_first_match prop =
+    match !matched, matcher prop with
+    | Some _, _ -> Some prop
     | None, (Some _ as value) ->
-      value_ref := value;
+      matched := value;
       None
+    | None, None -> Some prop
   in
-  let props = Prop_set.filter_map g props in
-  Option.map (fun value -> value, props) !value_ref
+  let props = Prop_set.filter_map choose_first_match props in
+  Option.map (fun value -> value, props) !matched
 
 let pop_bottom =
   pop_matched (function
-    | Prop.Bottom -> Some ()
+    | Bottom -> Some ()
     | _ -> None)
 
 let pop_not =
   pop_matched (function
-    | Prop.Not p -> Some p
+    | Not p -> Some p
     | _ -> None)
 
 let pop_and =
   pop_matched (function
-    | Prop.And (p1, p2) -> Some (p1, p2)
+    | And (p1, p2) -> Some (p1, p2)
     | _ -> None)
 
 let pop_or =
   pop_matched (function
-    | Prop.Or (p1, p2) -> Some (p1, p2)
+    | Or (p1, p2) -> Some (p1, p2)
     | _ -> None)
 
 let pop_imp =
   pop_matched (function
-    | Prop.Imp (p1, p2) -> Some (p1, p2)
+    | Imp (p1, p2) -> Some (p1, p2)
     | _ -> None)
 
-let rec prove seq = List.find_map (fun rule -> rule seq) rules
-and rules = [ axiom1; axiom2; notl; notr; andl; orr; impr; andr; orl; impl ]
+let rec prove seq = List.find_map (( |> ) seq) rules
 
-and axiom1 ((psl, psr) as seq) =
-  if Prop_set.disjoint psl psr then None else Some (Derivation.Axiom seq)
+and rules =
+  [ axiom1; axiom2; not_l; not_r; and_l; or_r; imp_r; and_r; or_l; imp_l ]
 
-and axiom2 ((psl, _) as seq) =
-  let* _ = pop_bottom psl in
+and axiom1 ((lhs, rhs) as seq) =
+  if Prop_set.disjoint lhs rhs then None else Some (Derivation.Axiom seq)
+
+and axiom2 ((lhs, _) as seq) =
+  let* _ = pop_bottom lhs in
   return (Derivation.Axiom seq)
 
-and notl ((psl, psr) as seq) =
-  let* p, psl = pop_not psl in
-  let seq' = psl, Prop_set.add p psr in
+and not_l ((lhs, rhs) as seq) =
+  let* p, lhs = pop_not lhs in
+  let seq' = lhs, Prop_set.add p rhs in
   let* deriv = prove seq' in
   return (Derivation.Not_l (seq, deriv))
 
-and notr ((psl, psr) as seq) =
-  let* p, psr = pop_not psr in
-  let seq' = Prop_set.add p psl, psr in
+and not_r ((lhs, rhs) as seq) =
+  let* p, rhs = pop_not rhs in
+  let seq' = Prop_set.add p lhs, rhs in
   let* deriv = prove seq' in
   return (Derivation.Not_r (seq, deriv))
 
-and andl ((psl, psr) as seq) =
-  let* (p1, p2), psl = pop_and psl in
-  let psl = Prop_set.add p1 psl in
-  let psl = Prop_set.add p2 psl in
-  let seq' = psl, psr in
+and and_l ((lhs, rhs) as seq) =
+  let* (p1, p2), lhs = pop_and lhs in
+  let seq' = Prop_set.(lhs |> add p1 |> add p2), rhs in
   let* deriv = prove seq' in
   return (Derivation.And_l (seq, deriv))
 
-and andr ((psl, psr) as seq) =
-  let* (p1, p2), psr = pop_and psr in
-  let seq1 = psl, Prop_set.add p1 psr in
+and and_r ((lhs, rhs) as seq) =
+  let* (p1, p2), rhs = pop_and rhs in
+  let seq1 = lhs, Prop_set.add p1 rhs in
   let* deriv1 = prove seq1 in
-  let seq2 = psl, Prop_set.add p2 psr in
+  let seq2 = lhs, Prop_set.add p2 rhs in
   let* deriv2 = prove seq2 in
   return (Derivation.And_r (seq, deriv1, deriv2))
 
-and orl ((psl, psr) as seq) =
-  let* (p1, p2), psl = pop_or psl in
-  let seq1 = Prop_set.add p1 psl, psr in
+and or_l ((lhs, rhs) as seq) =
+  let* (p1, p2), lhs = pop_or lhs in
+  let seq1 = Prop_set.add p1 lhs, rhs in
   let* deriv1 = prove seq1 in
-  let seq2 = Prop_set.add p2 psl, psr in
+  let seq2 = Prop_set.add p2 lhs, rhs in
   let* deriv2 = prove seq2 in
   return (Derivation.Or_l (seq, deriv1, deriv2))
 
-and orr ((psl, psr) as seq) =
-  let* (p1, p2), psr = pop_or psr in
-  let psr = Prop_set.add p1 psr in
-  let psr = Prop_set.add p2 psr in
-  let seq' = psl, psr in
+and or_r ((lhs, rhs) as seq) =
+  let* (p1, p2), rhs = pop_or rhs in
+  let seq' = lhs, Prop_set.(rhs |> add p1 |> add p2) in
   let* deriv = prove seq' in
   return (Derivation.Or_r (seq, deriv))
 
-and impl ((psl, psr) as seq) =
-  let* (p1, p2), psl = pop_imp psl in
-  let seq1 = psl, Prop_set.add p1 psr in
+and imp_l ((lhs, rhs) as seq) =
+  let* (p1, p2), lhs = pop_imp lhs in
+  let seq1 = lhs, Prop_set.add p1 rhs in
   let* deriv1 = prove seq1 in
-  let seq2 = Prop_set.add p2 psl, psr in
+  let seq2 = Prop_set.add p2 lhs, rhs in
   let* deriv2 = prove seq2 in
   return (Derivation.Imp_l (seq, deriv1, deriv2))
 
-and impr ((psl, psr) as seq) =
-  let* (p1, p2), psr = pop_imp psr in
-  let psl = Prop_set.add p1 psl in
-  let psr = Prop_set.add p2 psr in
-  let seq' = psl, psr in
+and imp_r ((lhs, rhs) as seq) =
+  let* (p1, p2), rhs = pop_imp rhs in
+  let seq' = Prop_set.(add p1 lhs, add p2 rhs) in
   let* deriv = prove seq' in
   return (Derivation.Imp_r (seq, deriv))
